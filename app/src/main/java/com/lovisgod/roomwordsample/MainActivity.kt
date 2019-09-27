90667063b436e2307f416a9e607c9e7d65aea3db
Lovisgod <olifedayo94@gmail.com>
2019-09-27 14:52:28 +0100

    FT/setup db && setup recycler view

--- app/src/main/java/com/lovisgod/roomwordsample/MainActivity.kt
+++ app/src/main/java/com/lovisgod/roomwordsample/MainActivity.kt
@@ -1,30 +1,48 @@
 package com.lovisgod.roomwordsample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // instantiate the viewModel class
+    private lateinit var wordViewModel: WordViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // set up recyclerView and adapter
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        // Use ViewModelProviders to associate your ViewModel with your Activity.
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // The onChanged() method fires when the observed data
+            // changes and the activity is in the foreground.
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
         }
     }
 
@@ -42,5 +60,27 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    // get the result from the intent
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.let {
+                val word = Word(it.getStringExtra(NewWordActivity.EXTRA_REPLY))
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // companion object works like our static variables anc class in JAVA
+    companion object {
+        const val newWordActivityRequestCode = 1
     }
 }
--- app/src/main/java/com/lovisgod/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/com/lovisgod/roomwordsample/NewWordActivity.kt
@@ -1,7 +1,11 @@
 package com.lovisgod.roomwordsample
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
 import android.widget.EditText
 
 class NewWordActivity : AppCompatActivity() {
@@ -11,5 +15,25 @@ class NewWordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_word)
         editWordView = findViewById(R.id.edit_word)
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            // MainActivity is sending an intent
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView.text)) {
+                // set the result for the intent and send back to main activity
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                // get the text from the edit text and parse to string
+                val word = editWordView.text.toString()
+                // add a string extra to the intent
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                // set the result for the intent and send back to main activity
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
     }
 }
